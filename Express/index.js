9ccc86d3c5b6f670b13a555f0c2eb8b99b9fadec
Abhinav <abhinav@AVs-Mac.local>
2023-09-20 00:15:30 +0530

    [19 sep 2023] express get req, params, query params, env variables

--- Express/index.js
+++ Express/index.js
@@ -3,27 +3,57 @@ const express =  require('express');
 const app = express();
 
 const products = [
-    {id: 101, name: 'Mobile', price: 20000},
-    {id: 102, name: 'Clothing', price: 250},
-    {id: 103, name: 'appliances', price: 5000},
-    {id: 104, name: 'grocery', price: 54500},
+    {id: 101, type: 'Mobile', price: 20000},
+    {id: 106, type: 'Mobile', price: 25000},
+    {id: 107, type: 'Mobile', price: 5000},
+    {id: 102, type: 'Clothing', price: 250},
+    {id: 103, type: 'appliances', price: 5000},
+    {id: 104, type: 'grocery', price: 800},
+    {id: 105, type: 'grocery', price: 500},
 ];
 
 app.get('/', (req, res) => {
     console.log('running get req for path /');
-    res.send('<h2>Home Page!!</h2>')
+    console.log('method', req.method);
+    console.log('path', req.path);
+    console.log('query', req.query);
+    res.send('<h2>Home Page!!</h2>');
 })
 
-app.get('/profile', (req, res) => {
-    console.log('profile page');
-    res.send('<h2>Profile Page!!</h2>')
+app.post('/', (req, res) => {
+    res.send('post request recieved')
+})
+
+app.get('/profile/:userId/:username', (req, res) => {
+    const {userId, username} = req.params
+    res.send(`<h2>Profile Page!! user Id: ${userId} username is ${username}</h2>`)
 })
 
 app.get('/products', (req, res) => {
-    res.send({total: products.length, products});
+    const {type, max} = req.query;
+    console.log('type', type);
+    console.log('max', max);
+    let resultProduct = []
+    if(type){
+        
+        resultProduct = products.filter((product) => {
+            return product.type === type
+        })
+    }
+    if(max){
+        console.log('max', max);
+        console.log(typeof max);
+        resultProduct = resultProduct.filter((product) => {
+            return product.price <= parseInt(max);
+        })
+    }
+    // res.status(404)
+    res.send({total: resultProduct.length, resultProduct});
 })
 
 const PORT = 4500;
 app.listen(PORT, () => {
     console.log(`App running on PORT: ${PORT}`);
 })
+
+console.log(__dirname);
